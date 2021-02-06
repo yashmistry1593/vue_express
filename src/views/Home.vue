b6d6b704aca22d1d3bcd11460090200e9696aa30
yashmistry1593 <yashmistry1593@gmail.com>
2021-02-06 14:08:14 +0530

    homepage changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
-  WELCOME TO HOME PAGE
+    WELCOME TO HOME PAGE
+    <ul>
+      <li>vue js</li>
+      <li>Express js</li>
+    </ul>
   </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
